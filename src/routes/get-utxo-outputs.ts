c3d19e07e3057f2e5529ef79f868970e9c9de038
enxtur <enxtur.mon@gmail.com>
2023-02-16 17:10:29 +0000

    changes route path utxo-outputs to utxo

--- src/routes/get-utxo-outputs.ts
+++ src/routes/get-utxo-outputs.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { bitcoinClient } from "../libs/bitcoin-core";
 
-export const getUtxoOutputsRouter = Router().get("/utxo-outputs/:address", async (req, res) => {
+export const getUtxoOutputsRouter = Router().get("/utxo/:address", async (req, res) => {
   const { address } = req.params;
   try {
     const result = await bitcoinClient.listUnspent(0, 9999999, [address]);
