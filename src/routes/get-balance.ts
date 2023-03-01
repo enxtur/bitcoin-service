c06c97653887d23e8c62f0a50ef9f35175b0b41d
enxtur <enxtur.mon@gmail.com>
2023-03-01 07:44:15 +0000

    decreases total confirmation of balance to 1

--- src/routes/get-balance.ts
+++ src/routes/get-balance.ts
@@ -3,7 +3,7 @@ import { bitcoinClient } from "../libs/bitcoin-core";
 
 export const getBalanceRouter = Router().get("/balance/:address", async (req, res) => {
   try {
-    const balance = await bitcoinClient.getReceivedByAddress(req.params.address, 6);
+    const balance = await bitcoinClient.getReceivedByAddress(req.params.address, 1);
     res.json({ balance });
   } catch(e: any) {
     res.status(500).json({ error: e.message });
